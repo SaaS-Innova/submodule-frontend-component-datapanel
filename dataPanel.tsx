a849a929b7f69d2669505a9e00d88ed233adf7e2
Nayan Gajera <nayan@saasinnova.com>
2025-04-01 10:33:25 +0530

    Add capitalize classname.

--- dataPanel.tsx
+++ dataPanel.tsx
@@ -7,7 +7,9 @@ const DataPanel: React.FC<IDataPanel> = ({ config, data }) => {
   const renderRowValues = (values: IDataPanelValue[]) =>
     values?.map((value, valueIndex) => (
       <div key={valueIndex} className="field grid">
-        <label className="col-12 mb-3 md:col-3 md:mb-0">{value.label}</label>
+        <label className="col-12 mb-3 md:col-3 md:mb-0">
+          <span className="capitalize-first">{value.label}</span>
+        </label>
         <div className="pl-3 col-12 md:col-9 relative">{value.value}</div>
       </div>
     ));
@@ -16,7 +18,9 @@ const DataPanel: React.FC<IDataPanel> = ({ config, data }) => {
   const renderColumnValues = (values: IDataPanelValue[]) =>
     values?.map((value, valueIndex) => (
       <div key={valueIndex} className="field flex justify-content-between">
-        <div className="col-6 p-0">{value.label}</div>
+        <label className="col-6 p-0 ">
+          <span className="capitalize-first">{value.label}</span>
+        </label>
         <div className="col-6 p-0 pl-3 relative">{value.value}</div>
       </div>
     ));
@@ -28,7 +32,7 @@ const DataPanel: React.FC<IDataPanel> = ({ config, data }) => {
           <div key={index}>
             {row.header && (
               <div>
-                <div>{row.header}</div>
+                <div className="capitalize-first">{row.header}</div>
                 <hr />
               </div>
             )}
@@ -48,7 +52,7 @@ const DataPanel: React.FC<IDataPanel> = ({ config, data }) => {
         <div key={index} className="col-4 p-0 pr-2">
           {row.header && (
             <div>
-              <div>{row.header}</div>
+              <div className="capitalize-first">{row.header}</div>
               <hr className="m-0 mb-2" />
             </div>
           )}
